45cbac96275e80cf3d3748fb47142c2ed29487bf
Mohamed Elmekawy <117996334+Mekkawy99@users.noreply.github.com>
2024-11-08 17:48:01 +0100

    Create seq_item.sv
    Running this testbench at this commit will print the clock generation pattern. Use EDA playground for that.

---
+++ dff_uvm/seq_item.sv
@@ -0,0 +1,11 @@
+// Sequence item
+
+class dff_seq_item extends uvm_seq_item;
+`uvm_object_utils(dff_seq_item) // sequence is not a component but it is an object.
+
+function new(string name = "dff_seq_item"); // No parent as it is not a component.
+    super.new(name);
+    `uvm_info("Sequence item class", "Constructor", UVM_MEDIUM)
+endfunction
+
+endclass
